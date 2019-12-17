80254a15dea0f49cc0059038cf62c474dee3ba78
mentalK94 <doingnow94@gmail.com>
2019-12-17 18:36:15 +0900

    XML에 외부properties 가져오기 실습

--- src/main/java/bean/ex4/ExternalFileEx.java
+++ src/main/java/bean/ex4/ExternalFileEx.java
@@ -1,4 +1,54 @@
 package bean.ex4;
 
-public class ExternalFileEx {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class ExternalFileEx implements InitializingBean, DisposableBean {
+
+    private String id;
+    private String password;
+    private String extId;
+    private String extPassword;
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy() call");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet() call");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getExtId() {
+        return extId;
+    }
+
+    public void setExtId(String extId) {
+        this.extId = extId;
+    }
+
+    public String getExtPassword() {
+        return extPassword;
+    }
+
+    public void setExtPassword(String extPassword) {
+        this.extPassword = extPassword;
+    }
 }
--- src/main/java/bean/ex4/MainExt.java
+++ src/main/java/bean/ex4/MainExt.java
@@ -1,4 +1,19 @@
 package bean.ex4;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainExt {
+
+    public static void main(String[] args) {
+        AbstractApplicationContext abstractApplicationContext = new GenericXmlApplicationContext("classpath:ext.xml");
+        ExternalFileEx externalFileEx = abstractApplicationContext.getBean("externalFileEx", ExternalFileEx.class);
+
+        System.out.println("envId : " + externalFileEx.getId());
+        System.out.println("envPW : " + externalFileEx.getPassword());
+        System.out.println("extId : " + externalFileEx.getExtId());
+        System.out.println("extPW : " + externalFileEx.getExtPassword());
+
+        abstractApplicationContext.close();
+    }
 }
